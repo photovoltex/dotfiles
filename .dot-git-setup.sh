244fc6e4e4d4b9a3a78071b8a37e99c8139f3796
photovoltex <36958734+photovoltex@users.noreply.github.com>
2021-09-26 00:12:04 +0200

    updated missing checkout

--- .dot-git-setup.sh
+++ .dot-git-setup.sh
@@ -2,6 +2,7 @@ git init
 git branch -m main
 git remote add origin https://github.com/photovoltex/dotfiles.git
 git fetch
+git checkout -t origin/main
 git submodule update --init
 chsh -s $(which zsh)
 source .zshrc
