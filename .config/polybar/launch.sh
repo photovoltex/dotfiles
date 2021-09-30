2ae6e7015fa3a9ef2cba1d9148b12208a8ec43e1
photovoltex <36958734+photovoltex@users.noreply.github.com>
2021-09-30 11:23:33 +0200

    polybar multimonitor script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,12 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Polybar launched..."
